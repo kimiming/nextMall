17f0a48bd57ae8499d711f6135c93f2975026835
kimiming <635596535@qq.com>
2026-01-07 21:23:34 +0800

    loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,5 +27,40 @@ export default function Home() {
         }
     }, [router]);
 
-    return null;
+    return (
+        <div
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: '100vh',
+                // background: '#f5f6fa',
+            }}
+        >
+            <div
+                style={{
+                    width: 48,
+                    height: 48,
+                    border: '6px solid #eee',
+                    borderTop: '6px solid  #b9ecdcff',
+                    borderRadius: '50%',
+                    animation: 'spin 1s linear infinite',
+                }}
+            />
+            <div style={{ marginTop: 24, fontSize: '1.2em', color: '#555' }}>
+                Redirecting, please wait...
+            </div>
+            <style jsx>{`
+                @keyframes spin {
+                    0% {
+                        transform: rotate(0deg);
+                    }
+                    100% {
+                        transform: rotate(360deg);
+                    }
+                }
+            `}</style>
+        </div>
+    );
 }
